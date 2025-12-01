efc7fdac023454ca42378d10e41ea5068aec63c5
IrisLei2808 <duclt2808@gmail.com>
2025-11-30 19:02:23 -0800

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Owner on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
